5a0bd68faff0d5f97f73c8c7ebe8f646cdaee049
ponkumarpandian <ponkumarpandian@gmail.com>
2018-04-04 17:22:56 +0530

    Updated controller

--- MyFirstWebAPI/MyFirstWebAPI/Controllers/ValuesController.cs
+++ MyFirstWebAPI/MyFirstWebAPI/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace MyFirstWebAPI.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] {"Blue","Red","Green","Yellow","White"};
+            return new string[] {"Blue","Red"};
         }
 
         // GET api/values/5
